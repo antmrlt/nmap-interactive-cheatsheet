e784c63a8beff8cd7448f9af07cf9950643d8ba7
antmrlt <antoine.mourlot@proton.me>
2024-01-18 13:18:50 +0100

    improve design

---
+++ backup/README.md
@@ -0,0 +1,17 @@
+# Nmap interactive cheatsheet
+
+![Alt text](image/image.png)
+
+Welcome to the Nmap Interactive Cheatsheet! This HTML document provides an interactive interface for generating Nmap commands with various options. You can customize your Nmap scans by selecting different scan techniques, host discovery methods, port specifications, and more.
+
+## Access
+
+Access via this url `https://antmrlt.github.io/nmap-interactive-cheatsheet/`
+
+## Notes
+
+- This cheatsheet is designed for educational purposes and may not cover all possible options or scenarios.
+- Always ensure that you have the right permissions before scanning any network or system.
+- Use Nmap responsibly and adhere to applicable laws and regulations.  
+
+Feel free to contribute to this cheatsheet by creating pull requests or suggesting improvements. Happy scanning!
---
+++ backup/script/script.js
@@ -0,0 +1,101 @@
+/* Menu déroulant */
+
+function toggleCheckboxes(headerId) {
+    const checkboxes = document.getElementById(headerId).parentElement.querySelectorAll('.checkboxes');
+    
+    for (const checkbox of checkboxes) {
+        checkbox.style.display = (checkbox.style.display === 'none' || checkbox.style.display === '') ? 'flex' : 'none';
+    }
+}
+
+
+for (let i = 1; i <= 12; i++) {
+    const header = document.getElementById(i);
+
+    header.addEventListener('click', () => {
+        toggleCheckboxes(i);
+    });
+}
+  
+/* Append parameter */
+
+function updateScanOptions(checkbox) {
+    var scanOptionsInput = document.getElementById("myInput");
+
+    if (checkbox.checked) {
+        scanOptionsInput.value += " -" + checkbox.id;
+        var checkboxesToDelete = document.getElementsByClassName('adelete');
+        if (checkboxesToDelete.length > 0) {
+            for (var i = 0; i < checkboxesToDelete.length; i++) {
+                if (checkboxesToDelete[i].checked){
+                    checkboxesToDelete[i].checked = false;
+                    scanOptionsInput.value = "nmap 192.168.1.1 -" + checkbox.id;
+                }
+            }
+        }
+    } else {
+        scanOptionsInput.value = scanOptionsInput.value.replace(" -" + checkbox.id, "");
+    }
+}
+
+
+function outcommand(checkbox) {
+    var scanOptionsInput = document.getElementById("myInput");
+
+    if (checkbox.checked) {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(function (otherCheckbox) {
+            if (otherCheckbox !== checkbox) {
+                otherCheckbox.checked = false;
+            }
+        });
+
+        scanOptionsInput.value = checkbox.id;
+    } else {
+        scanOptionsInput.value = scanOptionsInput.value.replace(checkbox.id, "nmap 192.168.1.1");
+    }
+}
+
+function nseuseful(checkbox) {
+    var scanOptionsInput = document.getElementById("myInput");
+
+    if (checkbox.checked) {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(function (otherCheckbox) {
+            if (otherCheckbox !== checkbox) {
+                otherCheckbox.checked = false;
+            }
+        });
+
+        scanOptionsInput.value = "nmap 192.168.1.1 -" + checkbox.id;
+    } else {
+        scanOptionsInput.value = scanOptionsInput.value.replace("nmap 192.168.1.1 -" + checkbox.id, "nmap 192.168.1.1");
+    }
+}
+
+
+/* Copy clipboard */
+
+function Copy() {
+    var copyText = document.getElementById("myInput");
+  
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+  
+    navigator.clipboard.writeText(copyText.value);
+    
+    alert("Copied the text: " + copyText.value);
+}
+
+/* Reset */
+
+function Reset() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+
+    var scanOptionsInput = document.getElementById("myInput");
+    scanOptionsInput.value = "nmap 192.168.1.1";
+}
