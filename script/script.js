06c3bd4f72714488d90903878bfac47d041f9e9d
antmrlt <antoine.mourlot@proton.me>
2024-04-11 19:41:40 +0200

    fix 2

--- script/script.js
+++ script/script.js
@@ -8,16 +8,18 @@ function toggleCheckboxes(headerId) {
     }
 }
 
-for (let i = 1; i <= idcompteur - 1; i++) {
-    const header = document.getElementById(i);
+// for (let i = 1; i <= idcompteur - 1; i++) {
+//     const header = document.getElementById(i);
 
-    header.addEventListener('click', () => {
-        toggleCheckboxes(i);
-        console.log('click' + i + idcompteur);
-    });
-}
+//     header.addEventListener('click', () => {
+//         toggleCheckboxes(i);
+//         console.log('click' + i + idcompteur);
+//     });
+// }
 
-toggleCheckboxes(1);
+addEventListener('click', () => {
+        toggleCheckboxes(1);
+});
   
 /* Append parameter */
 
